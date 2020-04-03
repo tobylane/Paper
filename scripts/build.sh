170047da52d80d70a1663894cf97b90d1eda34e5
Toby Lane <mrtobylane@gmail.com>
2020-04-03 14:51:15 +0100

    fixup! Initial shellcheck improvements

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-. "functions.sh"
+. "functions.sh" "$1"
 
 (
-($gitcmd submodule update --init && ./scripts/remap.sh "$basedir" && ./scripts/decompile.sh "$basedir" && ./scripts/init.sh "$basedir" && ./scripts/applyPatches.sh "$basedir") || (
+ ($gitcmd submodule update --init && ./scripts/remap.sh "$basedir" &&
+   ./scripts/decompile.sh "$basedir" &&
+   ./scripts/init.sh "$basedir" &&
+   ./scripts/applyPatches.sh "$basedir"
+ ) || (
     echo "Failed to build Paper"
     exit 1
-) || exit 1
+      ) || exit 1
 if [ "$2" = "--jar" ]; then
     mvn clean install && ./scripts/paperclip.sh "$basedir"
 fi
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -4,9 +4,17 @@ set -eo pipefail
 shopt -s nocasematch # Case insensitive comparisons
 
 gitcmd="git -c commit.gpgsign=false -c core.safecrlf=false"
-basedir="$(cd "$1" && pwd -P)"
-workdir="$basedir/work"
+
+setdir() {
+if [ -d "$1" ]; then
+  basedir="cd $1 && pwd -P"
+else
+  basedir="mkdir -p build; cd build && pwd -P"
+fi
+workdir="$($basedir)/work"
 minecraftversion=$(grep "$workdir"/BuildData/info.json -e minecraftVersion | cut -d '"' -f 4)
+}
+setdir "$@"
 
 # Windows detection to workaround ARG_MAX limitation
 if [ "$OSTYPE" = "cygwin" ] || [ "$OSTYPE" = "msys" ]; then
@@ -27,11 +35,11 @@ colorend() {
 }
 
 paperstash() {
-    STASHED=$($gitcmd stash  2>/dev/null || return 0) # errors are ok
+    STASHED=$($gitcmd stash 2>/dev/null || return 0) # errors are ok
 }
 
 paperunstash() {
-    if [ "$STASHED" != "No local changes to save" ] ; then
+    if [ "$STASHED" != "No local changes to save" ]; then
         $gitcmd stash pop 2>/dev/null || return 0 # errors are ok
     fi
 }
